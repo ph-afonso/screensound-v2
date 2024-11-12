30faf74390139cb5cb9ffeacfee0f4611ca8fb94
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2024-11-12 12:12:30 -0300

    Project

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,46 @@
+﻿Banda queen = new Banda("Queen");
+
+Album albumDoQueen = new Album("A night at the opera");
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+queen.ExibirDiscografia();
+
+Episodio ep1 = new(2, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("João");
+ep1.AdicionarConvidados("Bruno");
+
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Paulo");
+ep2.AdicionarConvidados("Carlos");
+ep2.AdicionarConvidados("Gui");
+
+Episodio ep3 = new(3, "Técnicas de programação", 67);
+ep3.AdicionarConvidados("Pedro");
+ep3.AdicionarConvidados("Natan");
+ep3.AdicionarConvidados("Tiago");
+
+Podcast podcast = new Podcast("PodCode", "Natanzin");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.AdicionarEpisodio(ep3);
+
+podcast.ExibirDetalhes();
+
